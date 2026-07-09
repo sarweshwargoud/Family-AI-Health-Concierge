6564fcbc82aca17df11e2f579b485c3bc738ab21
Sarweshwar Buddolla <127119085+sarweshwargoud@users.noreply.github.com>
2026-07-09 22:36:16 +0530

    Update FamilyStateContext.tsx

--- src/context/FamilyStateContext.tsx
+++ src/context/FamilyStateContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
-export interface FamilyMember {
+export interface FamilyMember{
   id: string;
   name: string;
   relation: string;
